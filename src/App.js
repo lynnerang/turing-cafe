2c8e67c4de6e91b8f4b88c01ef93407a91768b90
Lynne Rang <lynne.rang@gmail.com>
2019-05-16 10:04:36 -0600

    Add ReservationForm component with functionality that adds a new reservation.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 import ReservationContainer from './ReservationContainer';
+import ReservationForm from './ReservationForm';
 
 class App extends Component {
-  state = {
-    reservations: []
-  }
+	state = {
+		reservations: []
+	};
 
-  componentDidMount() {
-    fetch('http://localhost:3001/api/v1/reservations')
-      .then(response => response.json())
-      .then(reservations => this.setState({reservations}))
-  }
+	componentDidMount() {
+		fetch('http://localhost:3001/api/v1/reservations')
+			.then(response => response.json())
+			.then(reservations => this.setState({ reservations }));
+	}
 
-  render() {
-    console.log(this.state.reservations)
-    return (
-      <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
+	addReservation = details => {
+		this.setState({ reservations: [ ...this.state.reservations, details ] });
+	};
 
-        </div>
-        <div className='resy-container'>
-          <ReservationContainer reservations={this.state.reservations} />
-        </div>
-      </div>
-    )
-  }
+	render() {
+		console.log(this.state.reservations);
+		return (
+			<div className="App">
+				<h1 className="app-title">Turing Cafe Reservations</h1>
+				<div className="resy-form">
+					<ReservationForm addReservation={this.addReservation} />
+				</div>
+				<div className="resy-container">
+					<ReservationContainer reservations={this.state.reservations} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
