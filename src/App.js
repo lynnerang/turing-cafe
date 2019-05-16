6729cca0f8b4876b29a1537eafd8d7a89d27ea15
Lynne Rang <45218529+lynnerang@users.noreply.github.com>
2019-05-16 09:20:57 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className='app-title'>Turing Cafe Reservations</h1>
+        <div className='resy-form'>
+
+        </div>
+        <div className='resy-container'>
+          
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
